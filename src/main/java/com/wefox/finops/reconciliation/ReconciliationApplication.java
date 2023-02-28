98c87fe93805534a6f4c6929cda0949d0984a198
elaredo <65597821+elaredo@users.noreply.github.com>
2023-02-28 13:10:08 +0100

    Update ReconciliationApplication.java

--- src/main/java/com/wefox/finops/reconciliation/ReconciliationApplication.java
+++ src/main/java/com/wefox/finops/reconciliation/ReconciliationApplication.java
@@ -2,7 +2,8 @@ package com.wefox.finops.reconciliation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
 @SpringBootApplication
 public class ReconciliationApplication {
 
