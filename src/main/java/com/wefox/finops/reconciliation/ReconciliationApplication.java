c7d0492899da403e0696d002b2c9ab170b34f260
Oriol Canalias <oriol.canalias@wefox.com>
2023-02-28 12:09:27 +0100

    first commit

---
+++ src/main/java/com/wefox/finops/reconciliation/ReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.wefox.finops.reconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconciliationApplication.class, args);
+	}
+
+}
